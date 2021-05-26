b971610f0c62d8298bee9b11169f0b1bfcbc2506
Suratchanan Kraidech <benchbb01@hotmail.com>
2021-05-26 20:50:18 +0700

    reviewPage.js : Dynamic display data from databae (Except images)

--- Test/reviewPage.js
+++ Test/reviewPage.js
@@ -1,35 +1,52 @@
+const db = firebase.firestore().collection("review");
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-nav-toggle");
 navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
+   mainNav.classList.toggle("active");
+});
+var slideIndexArr = [];
+var master_container = document.querySelector(".main_container");
+db.get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    var data = doc.data();
+    data.id = doc.id;
+    master_container.innerHTML += generateBoxReview(data);
+    if(data.images !== undefined && data.images.length > 0){
+      setInterval(() => {showSlides(data.id)}, 2000);
+    }
+  });
 });
 
-var slideIndex = 0;
-showSlides();
+function generateBoxReview(data){
+  var htmlBoxReview = "";
+  htmlBoxReview += '<div class="boxReview">';
+    htmlBoxReview += '<h2 class="reviewTitle">' + data.activityName + '</h2>';
+    htmlBoxReview += '<h3 class="reviewPlace">' + data.touristAttraction + '</h3>';
+    if(data.images !== undefined && data.images.length > 0){
+      slideIndexArr[data.id] = 0;
+      htmlBoxReview += '<div class="slideshow-container">';
+      for(var ind = 0;ind < data.images.length;ind ++){
+        htmlBoxReview += '<div class="mySlides'+ data.id +' fade">';
+          htmlBoxReview +=  '<div class="numbertext">' + ind +' / ' + data.images.length + '</div>';
+          htmlBoxReview +=  '<img src="img_review1.jpg" style="width:100%" height="500px"/>';
+        htmlBoxReview +=  '</div>';
+      }
+      htmlBoxReview += '</div>';
+    }
+    htmlBoxReview += '<div class="descriptText">';
+      htmlBoxReview += '<p class="desText">'+ data.descriptionReview +'</p>';
+    htmlBoxReview += '</div>';
+  htmlBoxReview += '</div>';
+  return htmlBoxReview;
+}
 
-function showSlides() {
+function showSlides(classid) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides" + classid);
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-var slideIndex2 = 0;
-showSlides2();
-
-function showSlides2() {
-  var i;
-  var slides2 = document.getElementsByClassName("mySlides2");
-  for (i = 0; i < slides2.length; i++) {
-    slides2[i].style.display = "none";  
-  }
-  slideIndex2++;
-  if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
-  slides2[slideIndex2-1].style.display = "block";  
-  setTimeout(showSlides2, 2000); // Change image every 2 seconds
+  slideIndexArr[classid]++;
+  if (slideIndexArr[classid] > slides.length) {slideIndexArr[classid] = 1}    
+  slides[slideIndexArr[classid]-1].style.display = "block";  
 }
